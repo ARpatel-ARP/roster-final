dfb90f61e05a2f4294b0dde4b672060f13795314
Ankit Raut <rautanky407@gmail.com>
2026-09-14 14:31:06 +0530

    samesite issue

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -8,7 +8,7 @@ const generateToken = (res, adminId) => {
   res.cookie('token', token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict',
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
     maxAge: 24 * 60 * 60 * 1000, // 1 day
   });
 
